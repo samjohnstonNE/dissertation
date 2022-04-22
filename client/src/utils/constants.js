f62bf9a3111fe9979a64e15bf8b7b1315a4bad36
SamwiseNE <66041804+SamwiseNE@users.noreply.github.com>
2022-04-22 17:56:03 +0100

    22/04/2022

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -3,7 +3,8 @@ import abi from './transactions.json';
 
 /* Solidity  */
 export const contractABI = abi.abi;
-export const contractAddress = '0x4E8C3ca9dffe77100941e6B747Dd0D4B8fA135c1'; //smart contract address created from hardhat - This contract was deployed on the Ropsten Network
+export const contractAddress = '0x2AC609251d39bf138048eBaC0DD364dE784dAcfe'; //smart contract address created from hardhat - This contract was deployed on the Ropsten Network
+
 
 
 /* Etherscan api key */
--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -3,38 +3,10 @@
 pragma solidity ^0.8.0;
 
 contract Transactions {
-    uint256 transactionCount;
-
     event Transfer(address from, address to, uint amount, string message, uint256 timestamp);
 
-    struct TransferStruct {
-        address sender;
-        address to;
-        uint amount;
-        string message;
-        uint256 timestamp;
-    }
-
-    TransferStruct[] transactions;
-
-
     function addToBlockchain(address payable to, uint amount, string memory message) public {
-        transactionCount += 1;
-        transactions.push(TransferStruct(msg.sender, to, amount, message, block.timestamp));
 
         emit Transfer(msg.sender, to, amount, message, block.timestamp);
     }
-
-
-    function getAllTransactions() public view returns (TransferStruct[] memory) {
-        return transactions;
-
-    }
-
-
-    function getTransactionCount() public view returns (uint256) {
-        return transactionCount;
-
-    }
-
 }
