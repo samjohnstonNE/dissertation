a75f31a8814cababa6012a0cf2e8d041bad13dbb
SamwiseNE <66041804+SamwiseNE@users.noreply.github.com>
2022-04-22 18:23:57 +0100

    22/04/2022

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,11 +1,8 @@
 import abi from './transactions.json';
 
-
 /* Solidity  */
 export const contractABI = abi.abi;
 export const contractAddress = '0x2AC609251d39bf138048eBaC0DD364dE784dAcfe'; //smart contract address created from hardhat - This contract was deployed on the Ropsten Network
-
-
 
 /* Etherscan api key */
 export const apiKey = "7DJNBGU957QN9Q62ZP2X25539G4I1U9V45";
